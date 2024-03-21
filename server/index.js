a2d5f48228ba1cb303fa3f5e4e21e3d66dc8c489
naveen <stupiddint@gmail.com>
2024-03-21 18:09:16 +0530

    chore: server and db connect

--- server/index.js
+++ server/index.js
@@ -1,14 +1,44 @@
 const express = require( 'express' );
-const client = require( './database/databasepg.js' );
+const pool = require( './src/database/pgConnection.js' );
+const dotenv = require('dotenv');
+const route = require('./src/route/route.js');
+const  monitorUsage  = require('./src/utils/monitor_cpu_usages.js');
+const { getUsers, getProcessInfo } = require('./src/model/queries.js');
+const addRows = require('./src/model/model.js');
+
+dotenv.config();
+const app = express();
+const port = 3000;
+
+app.use('/',route);
 
 
-client.connect();
 
-client.query(`SELECT * FROM users`, ( err, res )=>{
-    if(!err){
-        console.log( res.rows );
-    } else{
-        console.log(err.message );
-    }
-    client.end();
-})
+// setInterval(()=>{
+// monitorUsage().then(processInfo => {
+//     for (const proc of processInfo) {
+//         console.log("proc", proc);
+//     }
+//     // console.log("processInfo",processInfo);
+//   }).catch(error => {
+//     console.error('An error occurred while monitoring process usage:', error);
+//   });
+
+// },1000);
+
+addRows();
+// pool.query(getProcessInfo, ( err, res )=>{
+//     if(!err){
+//         console.log( res.rows );
+//     } else{
+//         console.log(err.message );
+//     }
+//     pool.end();
+// })
+
+    
+pool.connect().then(()=>{
+    console.log("database connected successfully")
+    app.listen(port, ()=> console.log( `Server is running on port ${port}`));
+
+}).catch(err =>  console.log("db connection error " , err));
