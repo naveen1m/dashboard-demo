9e4a587e8dee3ab516cf0142858983e62791ddc7
naveen <stupiddint@gmail.com>
2024-03-21 18:11:09 +0530

    feat: monito cpu usages

---
+++ server/src/utils/monitor_cpu_usages.js
@@ -0,0 +1,37 @@
+const si = require('systeminformation');
+
+async function getProcessInfo() {
+    const processes = await si.processes();
+    return processes.list.map(proc => ({
+        pid: proc.pid,
+        name: proc.name,
+        cpuUsage: proc.cpu.toFixed(2),
+        memUsage: proc.mem.toFixed(2)
+    }));
+}
+
+async function monitorUsage() {
+        const processInfo = [];
+        const maxProcesses = 100;
+        let count = 0;
+
+        const processes = await getProcessInfo();
+        
+        processes.forEach(proc => {
+            if (count < maxProcesses && proc.memUsage > 0) {
+              processInfo.push({
+                PID: proc.pid,
+                Name: proc.name,
+                CPU: `${proc.cpuUsage}%`,
+                Memory: `${proc.memUsage}%`
+              });
+              count++;
+            }
+          });
+          console.log("processInfo", processInfo)
+
+return processInfo;
+
+}
+
+module.exports = monitorUsage;
